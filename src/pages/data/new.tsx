ae2f7048c3100d4f0ee546f74c2c631ee4c6fb55
AndrewJBateman <gomezbateman@yahoo.com>
2021-11-27 21:03:20 +0100

    feat: added pages & UI data display

--- src/pages/data/new.tsx
+++ src/pages/data/new.tsx
@@ -1,18 +1,82 @@
-import { Card, Form } from 'semantic-ui-react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { Card, Form, Icon, Button } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
 
-export default function newPage() {
+import { Data } from 'src/interfaces/Data';
+import { Layout } from 'src/components/Layout';
+
+const inititalState = {
+  title: "",
+  description: "",
+};
+
+const NewPage = (): JSX.Element => {
+	const [data, setData] = useState<Data>(inititalState)
+	const router = useRouter();
+
+	const handleChange = ({
+		target: { name, value },
+	}: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+		setData({ ...data, [name]: value });
+
+	const createData = async (data: Data) => {
+		await fetch('http://localhost:3000/api/data', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+	};
+
+	const loadData = async (id: string) => {
+		const res = await fetch('http://localhost:3000/api/data/' + id);
+		const data = await res.json();
+		setData({ title: data.title, description: data.description });
+	};
+
+	const updateData = async (id: string, data: Data) => {
+		await fetch('http://localhost:3000/api/data' + id, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+	};
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		try {
+			if (router.query.id) {
+				console.log('updating');
+			} else {
+				await createData(data);
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		if (typeof router.query.id === 'string') loadData(router.query.id);
+	}, [router.query]);
+
 	return (
-		<div>
+		<Layout>
 			<Card>
 				<Card.Content>
-					<Form>
+					<Form onSubmit={handleSubmit}>
 						<Form.Field>
-							<label htmlFor="title">Title</label>
+							<label htmlFor="title">Title:</label>
 							<input
 								type="text"
 								placeholder="Add data title"
 								name="title"
-								autoFocus
+								onChange={handleChange}
+								value={data.title}
 							/>
 						</Form.Field>
 						<Form.Field>
@@ -22,11 +86,19 @@ export default function newPage() {
 								id="description"
 								rows={2}
 								placeholder="Write a Description"
+								onChange={handleChange}
+								value={data.description}
 							></textarea>
 						</Form.Field>
+						<Button>
+							<Icon name="save" />
+							Save
+						</Button>
 					</Form>
 				</Card.Content>
 			</Card>
-		</div>
+		</Layout>
 	);
-}
+};
+
+export default NewPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import { GetServerSideProps } from 'next';
+import { Grid, Button } from 'semantic-ui-react';
 import { Data } from 'src/interfaces/Data';
+import { useRouter } from 'next/router';
+
+import { DataList } from 'src/components/data/DataList';
+import { Layout } from 'src/components/Layout';
 interface Props {
 	data: Data[];
 }
-export default function index({ data }: Props) {
-	return <>{data.length === 0 ? <h2>There is no data</h2> : <h2>Data</h2>}</>;
-}
+const Home = ({ data }: Props) => {
+	const { push } = useRouter();
 
-export const getServerSideProps = async () => {
+	return (
+		<Layout>
+			{data.length === 0 ? (
+				<Grid
+					columns={3}
+					centered
+					verticalAlign="middle"
+					style={{ height: '70%' }}
+				>
+					<Grid.Row>
+						<Grid.Column>
+							<h2>No data</h2>
+							<Button onClick={() => push('/data/new')}>Add data</Button>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			) : (
+				<DataList data={data} />
+			)}
+		</Layout>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
 	const res = await fetch('http://localhost:3000/api/data');
 	const data = await res.json();
-  console.log('data: ', data)
 
 	return {
 		props: {
@@ -18,3 +44,5 @@ export const getServerSideProps = async () => {
 		},
 	};
 };
+
+export default Home;
